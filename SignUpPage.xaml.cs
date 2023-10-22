805c90a2e6d6152b20e1ddc15e704630ca670a2b
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-10-22 06:13:50 +0100

    This commit lays down the initial structure for the SignUp page within the BestShopFruit application.

---
+++ SignUpPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace BestShopFruit;
+
+public partial class SignUpPage : ContentPage
+{
+
+
+	public SignUpPage()
+	{
+		InitializeComponent();
+	}
+
+	private void SignInClicked(object sender, EventArgs e)
+{
+    // Handle sign-in logic here
+	Navigation.PushAsync(new SignInPage());
+}
+}
+
