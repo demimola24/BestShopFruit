8905d9a792d45812c2e1897fa96cd2e91d3cbcab
Goonja <132929397+myCambrianGoonja@users.noreply.github.com>
2023-10-22 18:49:59 -0400

    Added MainPage.xml, mainPageViewModel
     This code includes includes a collection of "CarouselItem" objects, each representing a slide in an image carousel. Three initial slides are added in the constructor, each with a title, description, and an associated image. These slides promote "Best Shop Fruit," "Fresh & Healthy" options, and offer "Health Guide Tips." The ViewModel facilitates communication between the user interface and data, allowing the app to display these slides in a dynamic and user-friendly manner.

---
+++ MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestShopFruit
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        public ObservableCollection<CarouselItem> CarouselItems { get; set; } = new();
+        public MainPageViewModel()
+        {
+
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Best Shop Fruit",
+                Description = "Organic fruit enjoyed by everyone",
+                Image = "slide3.png"
+            });
+
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Fresh & Healthy",
+                Description = "Get fresh and healthy fruit for you",
+                Image = "slide2.png"
+            });
+
+            CarouselItems.Add(new CarouselItem()
+            {
+                Title = "Health Guide Tips",
+                Description = "Info on useful vitamins tips",
+                Image = "slide1.png"
+            });
+
+        }
+    }
+}
