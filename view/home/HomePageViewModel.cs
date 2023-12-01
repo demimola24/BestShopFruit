995e62b0ef6af7d7a03bb202d5c8dac66f9c9ece
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-12-01 15:04:49 -0500

    I've made several enhancements to the HomePageViewModel class responsible for managing interactions and data flow related to products, cart items, and order history in the BestShopFruit application. I've introduced tab navigation, added a relay command for product tapping, and ensured data initialization on the first run. The ViewModel now offers improved cart functionalities, including adding, deleting, and modifying quantities. Furthermore, it supports completing orders, updating the order history, and navigating to the OrderSuccessPage. These changes aim to provide a more robust and user-friendly shopping experience.

--- view/home/HomePageViewModel.cs
+++ view/home/HomePageViewModel.cs
@@ -1,21 +1,212 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
+using BestShopFruit.Models;
+using BestShopFruit.Services;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace BestShopFruit
+namespace BestShopFruit.View.Home
 {
-    public partial class HomePageViewModel : ObservableObject
+    [QueryProperty(nameof(Product), "Product")]
+    public partial class HomePageViewModel : BaseViewModel
     {
-        public ObservableCollection<CarouselItem> CarouselItems { get; set; } = new();
-        public HomePageViewModel()
+        private readonly AppService _appService;
+
+         public ObservableCollection<FruitModel> Products { get; } = new ObservableCollection<FruitModel>();
+
+         public ObservableCollection<FruitModel> CartItems { get; private set; } = new ObservableCollection<FruitModel>();
+
+         public ObservableCollection<OrderModel> OrderItems { get; private set; } = new ObservableCollection<OrderModel>();
+
+         
+
+        private bool isFirstRun;
+
+        [ObservableProperty]
+        FruitModel product;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomePageViewModel"/> class.
+        /// </summary>
+        /// <param name="appService">The authentication service.</param>
+        public HomePageViewModel(AppService appService)
         {
-
-        
-
+            _appService = appService;
+            isFirstRun = true;
         }
+
+        public HomePageViewModel(){}
+
+         [RelayCommand]
+        private async Task ProductTapped(FruitModel product)
+        {
+            if (product == null)
+            {
+                return;
+            }
+
+            var navigationParameter = new Dictionary<string, object>
+            {
+                { "Product", product },
+            };
+          
+
+            await Shell.Current.GoToAsync($"{nameof(ProductDetailsPage)}", true, navigationParameter);
+        }
+
+
+        [RelayCommand]
+         public async Task Init()
+        {
+            if (isFirstRun)
+            {
+               await GetProductsAsync();                                
+                isFirstRun = false;
+            }
+        }
+
+        [RelayCommand]
+        public async Task AddToCart(FruitModel product)
+        {
+            Debug.WriteLine("Called AddToCart");
+
+            if (IsBusy)
+            {
+                Debug.WriteLine($"Unable to delete cart: Is Busy");
+                return;
+            }
+
+            try
+            {
+               CartItems.Add(product);
+               await Shell.Current.DisplayAlert("Added", "Item has been added to cart:", "Ok");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to delete cart: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error", "Unable to delete cart:", "Ok");
+            }
+            finally
+            {
+                IsBusyWithCartModification = false;
+            }
+        }
+
+        private async Task GetProductsAsync()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+
+            try
+            {
+                IsBusy = true;
+
+                var products = await _appService.GetFruitsAsync();
+                Products.Clear();
+                foreach (var product in products)
+                {
+                    Products.Add(product);
+                }
+            }
+            catch (Exception ex)
+            {
+                 Debug.WriteLine($"Unable to get products: {ex.Message}");
+                  await Application.Current.MainPage.DisplayAlert("Error!", $"Unable to get products: {ex.Message}", "OK");   
+        
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+
+        [RelayCommand]
+        public async Task DeleteCart()
+        {
+            if (IsBusy || IsBusyWithCartModification)
+            {
+                return;
+            }
+
+            try
+            {
+               CartItems.Clear();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to delete cart: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error", "Unable to delete cart:", "Ok");
+            }
+            finally
+            {
+                IsBusyWithCartModification = false;
+            }
+        }
+
+        [RelayCommand]
+        public void IncreaseProductQuantity(FruitModel product)
+        {
+            try
+            {
+                var found = CartItems.Single(x=>x.id == product.id); 
+                Debug.WriteLine($"found: {found} with quantity {found.quantity}");
+                Debug.WriteLine($"Size: {CartItems.Count}");           
+                CartItems.Remove(product);
+                Debug.WriteLine($"Size After: {CartItems.Count}");
+                found.quantity = found.quantity+ 1;            
+                Debug.WriteLine($"Addeding : {found} with quantity {found.quantity}");
+                CartItems.Add(found);
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to increase product quantity: {ex.Message}");
+                Shell.Current.DisplayAlert("Error", "Unable to increase product quantity", "Ok");
+            }
+        }
+
+    
+        [RelayCommand]
+        public void DecreaseProductQuantity(FruitModel product)
+        {
+            try
+            {
+                var found = CartItems.Single(x=>x.id == product.id);            
+                CartItems.Remove(product);
+                found.quantity = found.quantity- 1;            
+                CartItems.Add(found);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to decrease product quantity: {ex.Message}");
+                Shell.Current.DisplayAlert("Error", "Unable to decrease product quantity", "Ok");
+            }
+        }
+
+        [RelayCommand]
+        public async Task CompleteOrder()
+        {
+            try
+            {
+                await Shell.Current.GoToAsync($"{nameof(OrderSuccessPage)}");  
+                var order = new OrderModel();            
+                order.id = $"Order-MBSF-{OrderItems.Count+1}";
+                order.quantity = $"{CartItems.Count+1} Items";
+                OrderItems.Add(order);
+                CartItems.Clear();                          
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to complete order: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error", "Unable to complete order ", "Ok");
+            }
+        }
+
+
     }
+    
 }
