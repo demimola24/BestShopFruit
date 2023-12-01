b7a0c4527796bc6e102ef0ca6ee2459b8287bf92
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-12-01 19:08:07 +0100

    Added ProductsPage with Dynamic UI Elements for Fruit App

---
+++ view/home/ProductsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BestShopFruit.View.Home;
+
+public partial class ProductsPage : ContentPage
+{
+	public ProductsPage()
+        {
+            InitializeComponent(); 
+            BindingContext = HomePage.homePageViewModel;      
+        }
+
+}
+
